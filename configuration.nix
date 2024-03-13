b23310ea2d2951d97435ecc401c836de56f8501d
dt@z3 <dt@z3>
2024-03-13 00:43:32 -0700

    host name

--- configuration.nix
+++ configuration.nix
@@ -39,7 +39,7 @@
 
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "z3"; # Define your hostname.
+  networking.hostName = "z1"; # Define your hostname.
 
   # Enable networking.
   # networking.useDHCP = true;
